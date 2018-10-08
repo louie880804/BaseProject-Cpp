9576556631419fac893827d186e1df3145840f37
陳毅 <charleslouie880804@gmail.com>
2018-10-08 19:53:28 +0800

    新增sub函式

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
     printf("%d\n", getTriangleType(3, 4, 5));
+    printf("%d\n",sub(5,3));
     return 0;
 }
 #endif // TEST
@@ -35,4 +36,8 @@ int getTriangleType(int a, int b, int c) {
         return -1;
     }
     return -3;
+}
+
+int sub(int a,int b){
+    return a-b;
 }
--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -5,8 +5,12 @@ void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
 }
 
+void testsub(){
+    assert(sub(5,3)==(2));
+}
 int main() {
     assert(addNumber(5, 2) == 7);
     testGetTriangleType();
+    testsub();
     return 0;
 }
